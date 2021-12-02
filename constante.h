58b8f7d7f68335cc591006660eb29ea609a2b4b2
Boubix88 <86961375+Boubix88@users.noreply.github.com>
2021-12-02 01:24:44 +0100

    Version 1 du 02/12/2021
    Ajout de la map en 3d, bug affichage en 3 fois, taille trop petite

--- constante.h
+++ constante.h
@@ -1,3 +1,4 @@
-#define WIDTH 512
+#define WIDTH 1024
 #define HEIGHT 512
 #define MAP_SIZE 16
+#define PLAYER_SIZE 8
--- createMap.c
+++ createMap.c
@@ -22,8 +22,11 @@ int main(void) {
                             "1     1      1 1",
                             "1111111111111111"}; 
   initialisationMap(img);
+  //initilaisationMap3D(img);
   drawWalls(map, img);
+  drawPlayer(img);
   drawFov(map, img);
+  draw3DMap(map, img);
   drawMap(img, file);
   
   (void) fclose(file);
@@ -41,23 +44,47 @@ void initialisationMap(unsigned char *img){
   }
 }
 
+/*void initilaisationMap3D(unsigned char *img){
+  for (int j = 0; j < HEIGHT; j++) {
+    for (int i = 0; i < WIDTH; i++) {
+        int idx = (i+j*WIDTH)*3;
+        for (int c=0; c<3; c++){
+          img[idx+c] = 255;
+        }
+    }
+  }
+}*/
+
 void drawWalls(const char map[16][16], unsigned char *img){
     for (int i=0; i<16; i++)
     for (int j=0; j<16; j++)
-      if (map[i][j]=='1') {
-          int x = i*WIDTH/16;
+      if (map[j][i]=='1') {
+          int x = i*(WIDTH/2)/16;
           int y = j*HEIGHT/16;
-        for (int a=0; a<WIDTH/16; a++)
+        for (int a=0; a<(WIDTH/2)/16; a++)
           for (int b=0; b<HEIGHT/16; b++) {
-          img[(x+a+(y+b)*WIDTH)*3] = 0;
+          img[(x+a+(y+b)*(WIDTH))*3] = 0;
           }
       }
 }
 
+void drawPlayer(unsigned char *img){
+  float player_x = 5*32;
+  float player_y = 6*32;
+  for (int i = 0; i < PLAYER_SIZE; i++){
+    for (int j = 0; j < PLAYER_SIZE; j++){
+      int idx = (player_x+i+(player_y+j)*(WIDTH))*3;
+      for (int c=0; c<3; c++){
+        img[idx+c] = 127;
+      }
+    }
+  }
+}
+
 void drawFov(const char map[16][16], unsigned char *img){
-  float player_x = 2;
-    float player_y = 7.5;
-    float player_a = 0.1789;
+  float player_x = 5;
+    float player_y = 6;
+    float player_a = 1.8;
     float player_fov = 1.58;
 
     for (int r=0; r<512; r++) { 
@@ -67,15 +94,51 @@ void drawFov(const char map[16][16], unsigned char *img){
         float cx = player_x + t*cos(angle);
         float cy = player_y + t*sin(angle);
 
-        if (map[(int)(cx)][(int)(cy)]!=' ') break;
+        if (map[(int)(cy)][(int)(cx)]!=' ') break;
 
-        int pixx = cx * WIDTH/16;
+        int pixx = cx * (WIDTH/2)/16;
         int pixy = cy * HEIGHT/16;
 
         for (int c=0; c<3; c++) {
-          img[(pixx + pixy*WIDTH)*3+c] = 0;
+          img[(pixx + pixy*(WIDTH))*3+c] = 0;
         }
     }
+    }
+}
+
+void draw3DMap(const char map[16][16], unsigned char *img){
+  float player_x = 5;
+  float player_y = 6;
+  float player_a = 1.8;
+  float player_fov = 1.58;
+
+  for (int r=0; r<512; r++) { 
+      float angle = player_a - player_fov/2 + player_fov*r/512;      
+    
+    for (float t=0; t<20; t+=.001) {
+        float cx = player_x + t*cos(angle);
+        float cy = player_y + t*sin(angle);
+
+        if (map[(int)(cy)][(int)(cx)]!=' '){
+          int line_height = HEIGHT/t;
+          /*for (int c=0; c<3; c++) {
+            //img[(r*2+line_height*WIDTH)*3+c] = 160;
+            img[(512+r+line_height*WIDTH)*3+c] = 160;
+          }*/
+          drawLine(img, WIDTH/2+r, HEIGHT*1.5-line_height/2, 1, line_height);
+          break;
+        }
+    }
+  }
+}
+
+void drawLine(unsigned char *img,float x, float y, float w, float h) {
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < h; j++) {
+            float cx = x + i;
+            float cy = y + j;
+            img[(int)cx + (int)cy*WIDTH] = 160;
+        }
     }
 }
 
